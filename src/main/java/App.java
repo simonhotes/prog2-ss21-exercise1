cfa23712cf114c1043fc462fb9987c1a5c4b4c72
simonhotes <92045737+simonhotes@users.noreply.github.com>
2022-03-21 20:12:21 +0100

    Setup finished, Tests missing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,46 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class App {
-    public static List<Integer> generateGrades() {
-        Random rand = new Random();
-        int upperbound = 60;
-        int listSize = rand.nextInt(60)+1;
-        List<Integer> grades = new ArrayList<>();
-        grades.add(listSize);
-        for (int i = 1; i < listSize; i++) {
-            int rand_int = rand.nextInt(81)+20;
-            grades.add(rand_int);
-        }
-        return grades;
+    public static boolean checkListSize(int size) {
+        return (0 <= size && size <= 60);
     }
-
-    public static List<Integer> finalGrades(List<Integer> grades){
-        List<Integer> finalGrades = new ArrayList<>();
-
-        for (int i = 1; i < grades.size(); i++) {
-            if (grades.get(i) >= 38) {
-                finalGrades.add(manipulateGrade(grades.get(i)));
-            } else {
-                finalGrades.add(grades.get(i));
-            }
-        }
-        return finalGrades;
+    public static boolean gradeRoundable(int grade) {
+        return (grade>=38);
     }
-
-    public static int manipulateGrade(int grade) {
-        if (grade % 5 == 3 || grade % 5 == 4) {
+    public static int roundGrade(int grade) {
+        if (App.gradeRoundable(grade) && (grade % 5 == 3 || grade % 5 == 4)) {
             grade = grade + (5 - (grade % 5));
         }
         return grade;
     }
+    public static boolean containsNullElement(List<Integer> grades) {
+        return grades.contains(null);
+    }
 
-    public static void main(String[] args) {
-        List<Integer> grades =  generateGrades();
-        List<Integer> finalGrades =  finalGrades(grades);
-        System.out.println(grades);
-        System.out.println(finalGrades);
-
+    public static List<Integer> finalGrades(List<Integer> grades) {
+        List<Integer> finalGrades = new ArrayList<>();
+        if(App.checkListSize(grades.get(0)) && !App.containsNullElement(grades)) {
+            for (int i = 1; i < grades.size(); i++) {
+                finalGrades.add(App.roundGrade(grades.get(i)));
+            }
+        }
+        return finalGrades;
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,14 +1,86 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    @Test
+    @DisplayName("List size below 0")
+    void checkListSize_below_0() {
+        assertFalse(App.checkListSize(-1));
+    }
 
     @Test
-    void printHelloWorld() {
-        App a1 = new App();
-        String expected = "Hello World.";
-        String actual = a1.printHelloWorld();
-        assertEquals(expected, actual);
+    @DisplayName("List size in range")
+    void checkListSize_inRange() {
+        assertTrue(App.checkListSize(60));
+    }
+
+    @Test
+    @DisplayName("List size higher 60")
+    void checkListSize_higher_60() {
+        assertFalse(App.checkListSize(-1));
+    }
+
+    @Test
+    @DisplayName("Grade not to round. Grade below 38")
+    void gradeRoundable_grade_lower_38() {
+        assertFalse(App.gradeRoundable(37));
+    }
+
+    @Test
+    @DisplayName("Grade to round. Grade equals 38")
+    void gradeRoundable_grade_equals_38() {
+        assertTrue(App.gradeRoundable(38));
+    }
+
+    @Test
+    @DisplayName("Grade not to round (grade=42)")
+    void roundGrade_grade_equals_42() {
+        assertEquals(42, App.roundGrade(42), "Rounding incorrect.");
+    }
+
+    @Test
+    @DisplayName("Grade to round (grade=43)")
+    void roundGrade_grade_equals_43() {
+        assertEquals(45, App.roundGrade(43), "Rounding incorrect.");
+    }
+
+    @Test
+    @DisplayName("Grade to round (grade=44")
+    void roundGrade_grade_equals_44() {
+        assertEquals(45, App.roundGrade(44), "Rounding incorrect.");
+    }
+
+    @Test
+    @DisplayName("Grade not to round (grade=45")
+    void roundGrade_grade_equals_45() {
+        assertEquals(45, App.roundGrade(45), "Rounding incorrect.");
+    }
+
+    @Test
+    @DisplayName("List contains null element")
+    void containsNullElement_with_null_element() {
+        List<Integer> grades = Arrays.asList(4,null,50);
+        assertTrue(App.containsNullElement(grades));
+    }
+
+    @Test
+    void containsNullElement_without_null_element() {
+        List<Integer> grades = Arrays.asList(4,87,50);
+        assertFalse(App.containsNullElement(grades));
+    }
+
+    @Test
+    @DisplayName("Correct building of final list")
+    void finalGrades() {
+        List<Integer> grades = Arrays.asList(4,73,67,38,33);
+        List<Integer> expected = Arrays.asList(75,67,40,33);
+        List<Integer> actual = App.finalGrades(grades);
+        assertEquals(expected, actual, "Test Failed");
     }
 }
