4dc12a381efdaa766c5fe6c326bc6db96838905b
simonhotes <92045737+simonhotes@users.noreply.github.com>
2022-03-21 20:37:38 +0100

    git repo link added to test class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+// Git-Repo: https://github.com/simonhotes/prog2-ss21-exercise1
 
 class AppTest {
     @Test
